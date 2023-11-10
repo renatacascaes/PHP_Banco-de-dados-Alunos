10f6f74e07372ae987c5e00b46368cebf9955c43
renatacascaes <126610357+renatacascaes@users.noreply.github.com>
2023-11-10 09:37:35 -0300

    integracao com BD

--- cadastro.php
+++ cadastro.php
@@ -7,19 +7,15 @@
     $senha = $_POST['senha'];
 
     try{
-    $sql = "insert into cliente (nome,email,dataNascimento,senha) values 
-    ('$nome','$email','$dataNascimento','$senha')";
-    mysqli_query($conexao,$sql);
-    print "<script>alert('Cadastrado com sucesso!')</script>";
+        $sql = "insert into cliente (nome,email,dataNascimento,senha) values 
+        ('$nome','$email','$dataNascimento','$senha')";
+        mysqli_query($conexao,$sql);
+        print "<script>alert('Cadastrado com sucesso!')
+        location.href='listarRegistros.php'
+        </script>
+        ";
 
     }catch(mysqli_sql_exception $e){
-        
+        print "<script>alert('Erro ao inserir o usuário')</script>";  
+        echo "Ocorreu um erro na consulta SQL: " . $e->getMessage();
     }
-    
-
-    
-
-    echo "Cadastrado com sucesso.";
-
-
-?>
---
+++ listarRegistros.php
@@ -0,0 +1,4 @@
+<?php
+echo "listar registros";
+
+?>
